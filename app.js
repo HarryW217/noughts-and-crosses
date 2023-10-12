daf461e9795f29b1e2b17bf27a7a4fcdb5e63f57
HarryW217 <harrywalker217@gmail.com>
2023-10-12 17:34:46 +0100

    Hide game over area and hover letters

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-// const gameBoard = document.querySelector("#gameboard");
-
-// const infoDisplay = document.querySelector("#info");
-
-// const startCells = ["", "", "", "", "", "", "", "", ""];
-
-// function createBoard() {
-//   startCells.forEach((cell, index) => {
-//     const cellElement = document.createElement("div");
-//     cellElement.classList.add("square");
-//     gameBoard.append(cellElement);
-//   });
-// }
-
-// createBoard();
+console.log("Hello there")
