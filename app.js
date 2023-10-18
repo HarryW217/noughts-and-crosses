7932087327601d606cc17464c1271362eb8ebe00
HarryW217 <harrywalker217@gmail.com>
2023-10-18 19:30:49 +0100

    Built a working noughts and crosses game!

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const strike = document.getElementById("strike");
 const gameOverArea = document.getElementById("game-over-area");
 const gameOverText = document.getElementById("game-over-text");
 const playAgain = document.getElementById("play-again");
+playAgain.addEventListener("click", startNewGame);
 
+//Sounds
 const gameOverSound = new Audio("sounds/game_over.wav");
 const clickSound = new Audio("sounds/click.wav");
 
@@ -66,7 +68,42 @@ function checkWinner() {
   for (const winningCombination of winningCombinations) {
     const { combo, strikeClass } = winningCombination;
     const tileValue1 = boardState[combo[0] - 1];
+    const tileValue2 = boardState[combo[1] - 1];
+    const tileValue3 = boardState[combo[2] - 1];
+    if (
+      tileValue1 !== null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3
+    ) {
+      strike.classList.add(strikeClass);
+      gameOverScreen(tileValue1);
+      return;
+    }
   }
+  //check for a draw
+  const allTilesFilledIn = boardState.every((tile) => tile !== null);
+  if (allTilesFilledIn) {
+    gameOverScreen(null);
+  }
+}
+
+function gameOverScreen(winnerText) {
+  let text = "Draw!";
+  if (winnerText !== null) {
+    text = `Winner is ${winnerText}!`;
+  }
+  gameOverArea.className = "visible";
+  gameOverText.innerText = text;
+  gameOverSound.play();
+}
+
+function startNewGame() {
+  strike.className = "strike";
+  gameOverArea.className = "hidden";
+  boardState.fill(null);
+  tiles.forEach((tile) => (tile.innerText = ""));
+  turn = PLAYER_X;
+  setHoverText();
 }
 
 const winningCombinations = [
